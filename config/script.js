ad3bdd4ee3eceee2f91102f1bec1f22f8cef824c
Kenn <141384556+Hirakushiii@users.noreply.github.com>
2024-07-14 23:25:26 +0700

    Update Backend

--- config/script.js
+++ config/script.js
@@ -70,27 +70,20 @@ function Video_fragment(m){
                 <div class="row g-0 text-center mt-4">
                     <div class="col-6 col-md-4 device-center">
                         <h4 class="salsa-font">Video Details:</h4>
-                        <img src="${m.author.avatar}" alt="" class="w-50 h-50 rounded-circle mb-2">
-                        <p class="account-name">${m.author.nickname}</p>
-                        <p class="vid-content">${m.desc}</p>
+                        <img src="${m.thumbnail}" alt="" class="w-50 h-50 rounded-circle mb-2">
+                        <p class="vid-content">${m.caption}</p>
                     </div>
                     <div class="col-sm-6 col-md-8">
                         <h4 class="salsa-font">Download Video:</h4>
                         <ul class="list-group">
                             <li class="list-group-item">
-                                <a href="${m.video1}" download="tiktokbykenn/lovyuuu!<3" class="btn bg-primary-subtle rounded my-1 w-50">DOWNLOAD VIDEO/MP4</a>
-                            </li>
-                            <li class="list-group-item">
-                                <a href="${m.video2}" download="tiktokbykenn/lovyuuu!<3" class="btn bg-primary-subtle rounded my-1 w-50">DOWNLOAD VIDEO/MP4 [2]</a>
+                                <a href="${m.video_sd}" download="tiktokbykenn/lovyuuu!<3" class="btn bg-primary-subtle rounded my-1 w-50">DOWNLOAD VIDEO/MP4</a>
                             </li>
                             <li class="list-group-item">
                                 <a href="${m.video_hd}" download="tiktokbykenn/lovyuuu!<3" class="btn bg-primary-subtle rounded my-1 w-50">DOWNLOAD VIDEO/MP4 [HD]</a>
                             </li>
                             <li class="list-group-item">
-                                <a href="${m.video_watermark}" download="tiktokbykenn/lovyuuu!<3" class="btn bg-primary-subtle rounded my-1 w-50">DOWNLOAD VIDEO/MP4 [WATERMARK]</a>
-                            </li>
-                            <li class="list-group-item">
-                                <a href="${m.music}" download="lovyuuu!<3"  class="btn bg-primary-subtle rounded my-1 w-50">DOWNLOAD AUDIO/MP3</a>
+                                <a href="${m.audio}" download="lovyuuu!<3"  class="btn bg-primary-subtle rounded my-1 w-50">DOWNLOAD AUDIO/MP3</a>
                             </li>
                         </ul>
                     </div>
